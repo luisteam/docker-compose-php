df68f084f496c06af49b67966ee9b26a8aab366e
luisteam <luis_rayita@hotmail.es>
2019-02-14 18:29:32 +0100

    v1

---
+++ php/Database.php
@@ -0,0 +1,25 @@
+<?php
+class Database {
+   public static $db;
+   public static $con;
+   function Database(){
+       $this->user=getenv('BOOKMEDIKBD_ENV_MYSQL_USER');$this->pass=getenv('BOOKMEDIKBD_ENV_MYSQL_PASSWORD');$this->host=getenv('BOOKMEDIKBD_PORT_3306_TCP_ADDR');$this->ddbb=getenv('BOOKMEDIKBD_ENV_MYSQL_DATABASE');
+
+   }
+
+   function connect(){
+       $con = new mysqli($this->host,$this->user,$this->pass,$this->ddbb);
+       $con->query("set sql_mode=''");
+       return $con;
+   }
+
+   public static function getCon(){
+       if(self::$con==null && self::$db==null){
+           self::$db = new Database();
+           self::$con = self::$db->connect();
+       }
+       return self::$con;
+   }
+
+}
+?>
